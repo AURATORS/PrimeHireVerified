e708070d0c154be4dea249f7fa66df4f58b048b5
Pranay Shinde <shindepranay039@gmail.com>
2025-05-26 20:02:16 +0530

    Adding files 2

---
+++ server/db/connect.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connect = async () => {
+    try {
+        console.log("Attenmpting to connect db...");
+        await mongoose.connect(process.env.MONGO_URI,{});
+        console.log("Conneted to Database..!");
+    } catch (error) {
+        console.log("Failed to connect to dbase",error.message);
+        process.exit(1);
+    }
+};
+
+
+export default connect;
